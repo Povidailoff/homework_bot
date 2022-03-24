5741b6612570d616630c643b1e746dfb6ea003ad
Евгений Повидайлов <povidailoff@yandex.ru>
2022-03-24 05:42:06 +0300

    Done?

--- homework.py
+++ homework.py
@@ -1,13 +1,19 @@
+import os
 import logging
 import time
 from http import HTTPStatus
 
-
+from dotenv import load_dotenv
 import telegram
 import requests
 
 import exceptions
 
+load_dotenv()
+
+PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 RETRY_TIME = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
